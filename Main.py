d34db300a6551de88ea45390c7d15444fa5f45f6
Derick <Drock3231@gmail.com>
2020-07-07 20:59:32 -0400

    smoll change

--- Main.py
+++ Main.py
@@ -4,9 +4,14 @@ from Objects import World
 from Objects import Creature
 
 # File will organize all the code and be the general script for running. Possibly have a mock game loop for running?
-# Python is pretty procedural from what I know but its possible there is some OOP support or that we can hack it togther
+# Python is pretty procedural from what I know but its possible there is some OOP support or that we can hack it together
 # with some clever file imports and arrays
 
 # initial vars?
-World = newWorld() # spitballing
-Creatures = newCreature() # spitballing until i write an object
+creatureNumber = 10
+World = World(10)
+Creatures = []
+
+for i in range(creatureNumber):
+    Creatures.append(Creature())
+
