9fedfa54e732c1a9b5a2de50362a0f0cea603606
Derick <Drock3231@gmail.com>
2020-07-07 20:23:05 -0400

    notes

---
+++ Main.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# File will organize all the code and be the general script for running. Possibly have a mock game loop for running?
+# Python is pretty procedural from what I know but its possible there is some OOP support or that we can hack it togther
+# with some clever file imports and arrays
