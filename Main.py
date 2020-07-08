7e698d891784845890446ce9dc37fd3bb6b3fc33
Derick <Drock3231@gmail.com>
2020-07-07 20:50:25 -0400

    adding files

--- Main.py
+++ Main.py
@@ -1,6 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from Objects import World
+from Objects import Creature
 
 # File will organize all the code and be the general script for running. Possibly have a mock game loop for running?
 # Python is pretty procedural from what I know but its possible there is some OOP support or that we can hack it togther
 # with some clever file imports and arrays
+
+# initial vars?
+World = newWorld() # spitballing
+Creatures = newCreature() # spitballing until i write an object
---
+++ Objects/World.py
@@ -0,0 +1,5 @@
+class World:
+
+    def __init__(self, sizeIn):
+        size = sizeIn
+        pass
